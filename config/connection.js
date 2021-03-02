545a13f2ab459494951d91c27b7dc14e174d9580
Blair Taylor <blairntaylor@gmail.com>
2021-03-01 19:06:11 -0700

    completed app setup and db setup - working through config setup

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+//setup MYSQL connection
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Daphne77",
+  database: "burgers_db",
+});
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for ORM
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Louisville", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hawaiian", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Taos", TRUE);
