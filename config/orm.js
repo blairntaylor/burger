2d9f6c3d1d8c645229c6aa8b6634d4fa291cdb0e
Blair Taylor <blairntaylor@gmail.com>
2021-03-01 21:42:00 -0700

    complete config section orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,68 @@
 // Import (require) `connection.js` into `orm.js`
-const connection = require("./connecion.js");
+const connection = require("./connection.js");
 
 //In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
-//  * `selectAll()`
-//  * `insertOne()`
-//  * `updateOne()`
+//print ??
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+    return arr.toString();
+  }
+
+
+//  * `selectAll()` -- read
+const orm = {
+    selectAll : (tableName, cb) => {
+        const queryString = `SELECT * FROM ${tableName}`;
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result)
+        })
+    }
+    //  * `insertOne()` -- create
+    insertOne: (tableName, cols, vals, cb) => {
+        const queryString = `INSERT INTO ${tableName}`;
+
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        console.log(queryString);
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result)
+        });
+    }
+    //  * `updateOne()` -- update
+    updateOne: (tableName, cols, vals, cb) => {
+        const queryString = `UPDATE ${tableName}`;
+
+        queryString += ' SET ';
+        queryString += objToSql(cols);
+        queryString += ' WHERE ';
+        queryString += vals;
+
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+          if (err) {
+            throw err;
+          }
+          cb(result)
+        });
+    }
+
+};
+
 
 // Export the ORM object in `module.exports`.
-
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(30) NOT NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
